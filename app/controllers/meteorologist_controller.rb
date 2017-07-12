24200fdeebd6fa60b1868ec51ed65ad73f0a8b42
elli olson <eolson10@chicagobooth.edu>
2017-07-12 20:01:06 +0000

    qa-each-component

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -14,19 +14,37 @@ class MeteorologistController < ApplicationController
     #
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
+    
+    # Get coordinates from Google maps
+    #
+    google_maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address.gsub(" ", "+")
+    
+    maps_results = JSON.parse(open(google_maps_url).read)
 
+    @lat = maps_results["results"][0]["geometry"]["location"]["lat"].to_s
+    
+    @lng = maps_results["results"][0]["geometry"]["location"]["lng"].to_s
+    
+    # Get coordinates from dark sky
+    #
+    dark_sky_url = "https://api.darksky.net/forecast/a52814fad0486c985720046e82a2e4fa/" + @lat + "," + @lng
+    
+    dark_sky_results = JSON.parse(open(dark_sky_url).read)
+    
+    # Return data for the page
+    #
+    @current_temperature = dark_sky_results["currently"]["temperature"].round(2)
 
+    @current_summary = dark_sky_results["currently"]["summary"]
 
-    @current_temperature = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = dark_sky_results["minutely"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_several_hours = dark_sky_results["hourly"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_days = dark_sky_results["daily"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
-
+    # Render the page
+    #
     render("meteorologist/street_to_weather.html.erb")
   end
 end
